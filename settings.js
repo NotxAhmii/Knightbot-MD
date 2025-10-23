ecfd70a142c758a210698f90a53b6b78d83e5b08
NotxAhmii <164718999+NotxAhmii@users.noreply.github.com>
2025-10-24 02:45:16 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Ahmii's Bot",
   botOwner: 'John', // Your name
-  ownerNumber: '923034819465', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '923174069008', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
